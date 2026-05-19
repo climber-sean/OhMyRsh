374f17e6edbc8aa16071651639f278cb809a6a2a
Sean Butlin <seanbutlin@googlemail.com>
2026-05-19 13:48:44 +0100

    wip: prompt saves to state

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -1,9 +1,21 @@
 import { forwardRef } from 'react';
 import styles from '../styles/promptinput.module.css';
 
-export const PromptInput = forwardRef<HTMLInputElement>((_props, ref) => {
+type PromptInputProps = {
+  onPromptSubmit: (e: React.SubmitEvent<HTMLFormElement>) => void;
+}
+
+export const PromptInput = forwardRef<HTMLInputElement, PromptInputProps>((props, ref) => {
+  const onSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    props.onPromptSubmit(event);
+  }
+
   return (
-    <input type="text" name="terminal-prompt" ref={ref} className={styles.promptInput} />
+    <form onSubmit={(e) => onSubmit(e)}>
+      <input type="text" name="terminal-prompt" ref={ref} autoComplete="off" className={styles.promptInput} />
+      <button type="submit">Submit</button>
+    </form>
   )
 });
 
--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,22 +1,46 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import styles from "../styles/window.module.css"
 import { PromptInput } from "./PromptInput"
 import { TerminalHeader } from "./TerminalHeader"
 import { PromptHeader } from "./PromptHeader";
 
 export const Window = () => {
+  const [prompts, setPrompts] = useState<string[]>([])
   const inputRef = useRef<HTMLInputElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const handleClick = () => {
     inputRef.current?.focus();
   };
 
+  const handlePromptSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(inputRef.current?.value);
+    setPrompts((prev) => [...prev, inputRef.current?.value as string]);
+    console.log(prompts);
+    console.log(containerRef);
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  }, [prompts])
+
   return (
     <div className={styles.window} onClick={handleClick} >
       <TerminalHeader />
-      <div style={{ padding: '10px' }}>
-        <PromptHeader />
-        <PromptInput ref={inputRef} />
+      <div ref={containerRef} style={{ padding: '10px' }}>
+        {prompts.map((value: string, i: number) => (
+          <div key={i}>
+            <PromptHeader />
+            <div>{value}</div>
+          </div>
+        ))}
+        <div>
+          <PromptHeader />
+          <PromptInput ref={inputRef} onPromptSubmit={handlePromptSubmit} />
+        </div>
       </div>
     </div>
   )
