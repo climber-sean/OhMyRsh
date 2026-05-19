fb52b1e6d0bf6a8e3fb14a2cff09d1243ab3f8d4
Sean Butlin <seanbutlin@googlemail.com>
2026-05-19 16:12:25 +0100

    wip: Added 2 basic terminal commands

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -15,7 +15,25 @@ export const Window = () => {
 
   const handlePromptSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setPrompts((prev) => [...prev, inputRef.current?.value as string]);  
+    setPrompts((prev) => [...prev, inputRef.current?.value as string]);
+    if (inputRef.current && commands[inputRef.current?.value]) {
+        commands[inputRef.current.value].commandFunc();
+    } 
+  }
+
+  const commands: Record<string, any> = {
+    "clear": {
+      helpMessage: "the clear command will empty your terminal screen, removing all previous output",
+      commandFunc: () => {
+        setPrompts([]);
+      }
+    },
+    "greet": {
+      helpMessage: "the greet command provides you with a nice friendly message",
+      commandFunc: () => {
+        setPrompts((prev) => [...prev, "Hello from OhMyRsh!"]);
+      }
+    }
   }
 
   useEffect(() => {
