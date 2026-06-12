8b8067c6532d069f1070ca4a2cdbaf5ce8ed404c
climber-sean <41762119+climber-sean@users.noreply.github.com>
2026-06-12 09:42:25 +0100

    chore: change build config (#19)

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -4,12 +4,13 @@ import dts from 'vite-plugin-dts';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), dts({ include: ["./src"], tsconfigPath: "./tsconfig.app.json" })],
+  plugins: [react(), dts({ include: ["./src"], tsconfigPath: "./tsconfig.app.json"})],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'OhMyRshTerminal',
       formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
