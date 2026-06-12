48d6f3929fb6f9e7cc14768edb75d7f6edb97834
Sean Butlin <seanbutlin@googlemail.com>
2026-06-12 09:16:18 +0100

    chore: fix errors

--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ["./src"], tsconfigPath: "./tsconfig.app.json" })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'OhMyRshTerminal',
       formats: ['es', 'cjs'],
-      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: { react: 'React', 'react-dom': 'ReactDOM' },
       },
