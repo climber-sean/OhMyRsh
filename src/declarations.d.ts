5f08e8c0a3f382be64da2ce843c39fbbe78f73f2
climber-sean <41762119+climber-sean@users.noreply.github.com>
2026-06-12 10:39:32 +0100

    Chore fix more build configs (#21)
    * chore(release): 0.1.5
    
    * chore: fixing build configs again

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,5 @@
 declare module '@fontsource/noto-mono';
+declare module '*.module.css' {
+  const styles: { [key: string]: string };
+  export default styles;
+}
--- vite.lib.config.ts
+++ vite.lib.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
+import dts from 'unplugin-dts/vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), dts({ include: ["./src"], tsconfigPath: "./tsconfig.app.json"})],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -18,5 +17,7 @@ export default defineConfig({
         globals: { react: 'React', 'react-dom': 'ReactDOM' },
       },
     },
+    emptyOutDir: true,
   },
+  plugins: [react(), dts({ include: ['./src'], tsconfigPath: "./tsconfig.app.json", outDirs: './dist', bundleTypes: true })],
 })
